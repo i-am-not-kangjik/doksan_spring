906d8f1e2b13243df8baf83f8c64c8fc25031769
Kangjik Kim <rkqqkb@gmail.com>
2023-06-25 13:19:10 +0900

    브랜치 꼬임 해결

--- src/main/java/kjkim/kjkimspring/controller/MessageRestController.java
+++ src/main/java/kjkim/kjkimspring/controller/MessageRestController.java
@@ -1,2 +1,50 @@
-package kjkim.kjkimspring.controller;public class MessageRestController {
+package kjkim.kjkimspring.controller;
+
+import kjkim.kjkimspring.dto.MessageDto;
+import kjkim.kjkimspring.message.Message;
+import kjkim.kjkimspring.message.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageRestController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping
+    public void sendMessage(@RequestBody MessageDto messageDto) {
+        messageService.sendMessage(messageDto.getSenderUsername(), messageDto.getReceiverUsername(), messageDto.getContent());
+    }
+
+    @PreAuthorize("#username == authentication.principal.username")
+    @GetMapping("/received/{username}")
+    public List<MessageDto> getReceivedMessages(@PathVariable String username) {
+        return messageService.getReceivedMessages(username);
+    }
+
+    @PreAuthorize("#username == authentication.principal.username")
+    @GetMapping("/sent/{username}")
+    public List<MessageDto> getSentMessages(@PathVariable String username) {
+        return messageService.getSentMessages(username);
+    }
+
+
+
+//    @PreAuthorize("#username == authentication.principal.username")
+//    @DeleteMapping("/received/{username}/{messageId}")
+//    public void deleteReceivedMessage(@PathVariable Long messageId, @PathVariable String username) {
+//        messageService.deleteReceivedMessage(messageId, username);
+//    }
+//
+//    @PreAuthorize("#username == authentication.principal.username")
+//    @DeleteMapping("/sent/{username}/{messageId}")
+//    public void deleteSentMessage(@PathVariable Long messageId, @PathVariable String username) {
+//        messageService.deleteSentMessage(messageId, username);
+//    }
 }
---
+++ src/main/java/kjkim/kjkimspring/message/MessageService.java
@@ -0,0 +1,73 @@
+package kjkim.kjkimspring.message;
+
+import kjkim.kjkimspring.dto.MessageDto;
+import kjkim.kjkimspring.user.User;
+import kjkim.kjkimspring.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class MessageService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public void sendMessage(String senderUsername, String receiverUsername, String content) {
+        Optional<User> sender = userRepository.findByUsername(senderUsername);
+        Optional<User> receiver = userRepository.findByUsername(receiverUsername);
+
+        if (sender.isPresent() && receiver.isPresent()) {
+            Message message = new Message();
+            message.setSender(sender.get());
+            message.setReceiver(receiver.get());
+            message.setContent(content);
+
+            messageRepository.save(message);
+        } else {
+            // handle the case when sender or receiver does not exist
+        }
+    }
+
+    public MessageDto getMessageById(Long messageId) {
+        Optional<Message> optionalMessage = messageRepository.findById(messageId);
+        if (optionalMessage.isPresent()) {
+            Message message = optionalMessage.get();
+            return new MessageDto(message);
+        } else {
+            // handle the case when message does not exist
+            return null;
+        }
+    }
+
+    public List<MessageDto> getReceivedMessages(String username) {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isPresent()) {
+            List<Message> messages = messageRepository.findByReceiver(user.get());
+            return messages.stream().map(MessageDto::new).collect(Collectors.toList());
+        } else {
+            // handle the case when user does not exist
+            return new ArrayList<>();
+        }
+    }
+
+    public List<MessageDto> getSentMessages(String username) {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isPresent()) {
+            List<Message> messages = messageRepository.findBySender(user.get());
+            return messages.stream().map(MessageDto::new).collect(Collectors.toList());
+        } else {
+            // handle the case when user does not exist
+            return new ArrayList<>();
+        }
+    }
+}
+
